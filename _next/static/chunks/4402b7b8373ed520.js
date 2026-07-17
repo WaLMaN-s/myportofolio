7d8c9f076b93dc0ccf92ed7abc2c65435d04b9e0
WaLMaN-s <WaLMaN-s@users.noreply.github.com>
2026-07-17 16:19:00 +0000

    deploy: f80fe421d7f599db0b4dec275e42686f7a9fae4c

---
+++ _next/static/chunks/4402b7b8373ed520.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/a7c9027351a0c160.js",
+  "static/chunks/turbopack-25799ada7df8829a.js"
+])
---
+++ _next/static/chunks/a2cd1b27196df894.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/a7c9027351a0c160.js",
+  "static/chunks/turbopack-985c09081a1b053b.js"
+])
