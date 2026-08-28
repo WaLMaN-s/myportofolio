0287e1f2e69f8f7381271c341ce89d3ffdf358b1
Walman <sswalman49@gmail.com>
2026-08-28 17:50:45 +0700

    certificate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
 import Experience from "@/components/Experience";
+import certificate from "@/components/certificate";
 import Skills from "@/components/Skills";
 import Blog from "@/components/Blog";
 import Contact from "@/components/Contact";
